c075583b404aabf96b9983839dcd6f31d6e6aedf
“agustinalonsocantoli” <agustinalonsocantoli@gmail.com>
2024-10-27 01:06:59 +0200

    changed git

--- src/app/aviso-legal/page.tsx
+++ src/app/aviso-legal/page.tsx
@@ -1,7 +1,12 @@
+import { Heading } from '@chakra-ui/react'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>aviso</div>
+    <div>
+      <Heading as="h1" size="xl" mb={4}>
+        Aviso legal
+      </Heading>
+    </div>
   )
 }
